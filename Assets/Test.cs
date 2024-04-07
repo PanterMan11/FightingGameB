7d2f22ed29b392868cf3af0926559fe39d022d5b
Bread <kaankocak@gmail.com>
2024-04-07 21:38:20 +0300

    test
    code that has not been debugged at all god bless please work

---
+++ Assets/Test.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float moveSpeed;
+    [SerializeField] SpriteRenderer sr;
+    public float jumpForce;
+    private float moveInput;
+    private bool isGrounded;
+    [SerializeField] Transform feetPos;
+    [SerializeField] float checkRadius;   
+    [SerializeField] LayerMask whatIsGround;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    
+    void Update()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        FaceMoveDirection();
+        Jump();
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+    }
+    void Jump()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        if(isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void FaceMoveDirection()
+    {
+
+        if(moveInput > 0)
+        {
+            sr.flipX = false;
+        }else if(moveInput < 0)
+        {
+            sr.flipX = true;
+        }
+
+        void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(feetPos.position, checkRadius);
+        }
+
+
+            
+
+    }
+}
