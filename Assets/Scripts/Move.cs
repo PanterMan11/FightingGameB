2ea273f0e14c7dfa8c4ef7cbeff46d506e6239b1
Bread <kaankocak@gmail.com>
2024-04-11 01:59:49 +0300

    dash fix
    dash fixed also has cooldown now may god help whoever reading this understaned what the code does

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -12,7 +12,6 @@ public class Move : MonoBehaviour
     public float jumpForce;
     private float moveInput;
     private bool isGrounded;
-
     bool canDash = true;
     bool isDashing ;
     [SerializeField] float dashingPower;
@@ -37,29 +36,50 @@ public class Move : MonoBehaviour
     void Update()
     {
 
-        
+
         if (isDashing)
         {
             return; // this is so the player is locked in a dash and cant fumble around while dashing 
         }
-
+        
         moveInput = Input.GetAxisRaw("Horizontal");
-        //  Debug.Log(moveInput);
         FaceMoveDirection();
         Jump();
-        if(Input.GetButtonDown("Horizontal"))
-        {
-            
+        if(Input.GetButtonDown("Horizontal")&& moveInput == 1 && canDash == true)
+        { // DASH RIGHT
             float timeSinceLastClick = Time.time - lastClickTime;
-            if(timeSinceLastClick <= DOUBE_TIME) {
-                Debug.Log("IDASH");
+            if(timeSinceLastClick <= DOUBE_TIME && moveInput == 1) {
+                Debug.Log("IDASH RIGHT");
                 StartCoroutine(Dash());
-            } else { 
-            
             }
 
-            Debug.Log(timeSinceLastClick);
+            //Debug.Log(timeSinceLastClick);
             lastClickTime = Time.time;
+        } 
+        else if (Input.GetButtonDown("Horizontal") && moveInput == -1 && canDash == true)
+        {
+            // DASH LEFT
+            float timeSinceLastClick = Time.time - lastClickTime;
+            if (timeSinceLastClick <= DOUBE_TIME && moveInput == -1)
+            {
+                Debug.Log("IDASH LEFT");
+                StartCoroutine(Dash());
+            }
+            //Debug.Log(timeSinceLastClick);
+            lastClickTime = Time.time;
+
+        } else if (Input.GetButtonDown("Jump") && canDash == true)
+        {
+            float timeSinceLastClick = Time.time - lastClickTime;
+            if (timeSinceLastClick <= DOUBE_TIME)
+            {
+                Debug.Log("IDASH WITH THE JUMP KEY");
+                StartCoroutine(Dash());
+            }
+            //Debug.Log(timeSinceLastClick);
+            lastClickTime = Time.time;
+
+
         }
     }
     private void FixedUpdate()
@@ -68,11 +88,18 @@ public class Move : MonoBehaviour
         {
             return; // this is so the player is locked in a dash and cant fumble around while dashing 
         }
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+        //Debug.Log("pls " + isGrounded);
+        if (canDash == false && isGrounded == true)
+        {
+            StartCoroutine(DashCooldown());
+        }
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
     }
     void Jump()
     {
-        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+       // isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround); 
 
         if (isGrounded == true && Input.GetButtonDown("Jump"))
         {
@@ -87,11 +114,11 @@ public class Move : MonoBehaviour
             if (jumpTime> 0) {
                 rb.velocity = Vector2.up * jumpForce;
                 jumpTime -= Time.deltaTime ;
-                Debug.Log(jumpTime);
+                //Debug.Log(jumpTime);
             }
             else { 
                 isJumping=false;
-                Debug.Log("BIIG JUMp NO ELSE");
+                //Debug.Log("BIIG JUMp NO ELSE");
             }
             
         }
@@ -99,24 +126,36 @@ public class Move : MonoBehaviour
         if (Input.GetButtonUp("Jump"))
         {
             isJumping = false;
-            Debug.Log("BIIG JUMp NO GETUP");
+            //Debug.Log("BIIG JUMp NO GETUP");
         }
     }
+
 
     private IEnumerator Dash()
     {
         canDash = false;
+        Debug.Log("cant dash dipshit");
         isDashing = true;
         float originalGravity = rb.gravityScale;
         rb.gravityScale = 0f;
         rb.velocity = new Vector2(moveInput* dashingPower, 0f);
-
         yield return new WaitForSeconds(dashingTime);
         rb.gravityScale = originalGravity;
         isDashing=false;
-        yield return new WaitForSeconds(dashingCooldown);
+
+        
     }
 
+    private IEnumerator DashCooldown()
+    {
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+        Debug.Log("can dash dipshit");
+
+    }
+
+
+    
 
 
 
